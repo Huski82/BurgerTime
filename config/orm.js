16909f0d476f7a3e24901b83dd6b8a2c5205334a
christiansuchoski <christiansuchoski@gmail.com>
2021-01-15 15:33:31 -0800

    preping orm.js and seed.sql file

--- config/orm.js
+++ config/orm.js
@@ -9,3 +9,21 @@ const orm = {
         })
     }
 }
+
+insertOne(table, cols, vals, cb) {
+    const query = `INSERT INTO (${table}) (${cols}) (${vals})`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        cb(res)
+    })
+},
+
+updateOne(table, cols, vals, condition, cb) {
+    const query = `UPDATE ${table} set ${cols} = ${vals} where ${condition}`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        cb(res)
+    })
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hamburger", false), ("Double Charburger", false), ("Bacon HeartAttack Cheeseburger", false); 
