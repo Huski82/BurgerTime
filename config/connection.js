f53f5e552c8bbca1769b56401471c5dfa8e36a9b
christiansuchoski <christiansuchoski@gmail.com>
2021-02-10 14:46:05 -0800

    added to connection.js/burger.js/server.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnect({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "password",
+    password: "emmahuski1",
     database: "burgers_db"
 })
 
