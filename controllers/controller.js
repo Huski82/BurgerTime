b5d0e4d9804d46420115d7b792e134afd23ddd59
christiansuchoski <christiansuchoski@gmail.com>
2021-02-11 10:44:25 -0800

    fixing paths

---
+++ controllers/controller.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const burger = require("../model/burger.js")
+
+router.get('/', (req,res) => {
+    burger.selectAll((data) => {
+        res.render('index', {burgers: data})
+    })
+});
